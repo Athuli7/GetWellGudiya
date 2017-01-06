e02e6ffb5e6238a0e616adc0ac48f0e17de5a68b
Athul Raj <athulbraj@gmail.com>
2017-01-06 10:38:44 +0530

    Server

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,3 +3,15 @@ $('#myTabs a').click(function (e) {
   e.preventDefault()
   $(this).tab('show')
 });
+
+$.ajax({
+	url: "stats.json",
+	success: function( result ) {
+		result.percent = result.collectedAmount/result.totalAmount*100;
+		result.percent = result.percent.toFixed(2);
+
+		var template = $('#contentr').html();
+		var rendered = Mustache.render(template, {data:result});
+		$('#contentr').html(rendered);
+	}
+});
