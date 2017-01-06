82010aecc67b4e20929e4c36a35e7913b83e124b
Athul Raj <athulbraj@gmail.com>
2017-01-06 11:28:51 +0530

    Server

--- assets/js/main5.js
+++ assets/js/main5.js
@@ -7,10 +7,9 @@ $('#myTabs a').click(function (e) {
 $.ajax({
 	url: "stats.json",
 	success: function( result ) {
-		result.percent = result.collectedAmount/result.totalAmount*100;
-		result.percent = result.percent.toFixed(2);
-		result.hours = ( (Date.now()/1000) - result.eventTime )/3600;
-		var ttR = ['#render1']
+		result.percent = (result.collectedAmount/result.totalAmount*100).toFixed(2);
+		result.hours = (((Date.now()/1000) - result.eventTime )/3600).toFixed(2);
+		var ttR = ['#render1','#render2']
 		for(var ttr in ttR){
 			var template = $(ttR[ttr]).html();
 			var rendered = Mustache.render(template, {data:result});
