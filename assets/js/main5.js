65721bc3daea27bf966ba420b8a5654bc5d7f559
Athul Raj <athulbraj@gmail.com>
2017-01-06 15:10:39 +0530

    Server

--- assets/js/main5.js
+++ assets/js/main5.js
@@ -8,7 +8,7 @@ $.ajax({
 	url: "stats.json",
 	success: function( result ) {
 		result.percent = (result.collectedAmount/result.totalAmount*100).toFixed(2);
-		result.hours = (((Date.now()/1000) - result.eventTime )/3600).toFixed(2);
+		result.hours = (((Date.now()/1000) - result.eventTime )/3600).toFixed(0);
 		var ttR = ['#render1','#render2']
 		for(var ttr in ttR){
 			var template = $(ttR[ttr]).html();
